7e90c84765105786ce289966b91f9233c7341dee
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2014-02-26 02:28:02 +0100

    Slick 2.0 support

--- sample/src/main/scala/Services.scala
+++ sample/src/main/scala/Services.scala
@@ -1,40 +1,83 @@
 import model.XDb._
+import org.joda.time.DateTime
+import scala.slick.jdbc.JdbcBackend
+import scala.slick.profile.RelationalTableComponent
 import scala.Some
-import slickmacros.reflect._
 import slickmacros.annotations._
-import slickmacros.dao.Crud._
 import slickmacros.Implicits._
 import scala.slick.driver.PostgresDriver.simple._
-import slickemf.export._
-import slickmacros._
-import scala.reflect.macros.Context
-import scala.annotation.StaticAnnotation
-import scala.reflect.runtime.universe._
 
-import scala.reflect.runtime.{ universe => u }
+import scala.reflect.runtime.{universe => u}
 import scala.language.experimental.macros
 import scala.language.dynamics
-import scala.slick.lifted.{ Query => LQuery }
-import scala.slick.lifted.ColumnBase
+import scala.slick.lifted.{Query => LQuery, MappedProjection}
 
 
 object Services {
+
+//  type TableEx[C] = {
+//    def id: Column[Long]
+//    def forInsert: MappedProjection[C, _]
+//  }
+//
+//  type RowId = {
+//    def id: Option[Long]
+//  }
+//
+//  type RowIdEx = {
+//    def id: Option[Long]
+//    var dateCreated: org.joda.time.DateTime
+//    var lastUpdated: org.joda.time.DateTime
+//  }
+//
+//
+//  trait Crud[C <: RowId, +T <: RelationalTableComponent#Table[C] with TableEx[C]] {
+//    self: TableQuery[T] =>
+//
+//    def del(objId: Long)(implicit session: JdbcBackend#SessionDef) = {
+//      self.where(_.id === objId)
+//    }
+//
+//    def find(objId: Long)(implicit session: JdbcBackend#SessionDef): Option[C] = self.where(_.id === objId).firstOption
+//
+//    def update(obj: C)(implicit session: JdbcBackend#SessionDef): Int = {
+//      val res = (for {row <- self if row.id === obj.id.get} yield row) update (obj)
+//      res
+//    }
+//
+//    def insert(obj: C)(implicit session: JdbcBackend#SessionDef) = {
+//      val res = self returning (self.map(_.id)) insert (obj)
+//      res
+//    }
+//  }
+//
+//  trait CrudEx[C <: RowIdEx, +T <: RelationalTableComponent#Table[C] with TableEx[C]] extends Crud[C, T] {
+//    self: TableQuery[T] =>
+//    override def update(obj: C)(implicit session: JdbcBackend#SessionDef): Int = {
+//      obj.lastUpdated = DateTime.now
+//      super.update(obj)
+//    }
+//
+//    override def insert(obj: C)(implicit session: JdbcBackend#SessionDef) = {
+//      // because x.copy(dateCreated = , lastUpdated = ) is not available :(
+//      obj.dateCreated = DateTime.now
+//      obj.lastUpdated = obj.dateCreated
+//      super.insert(obj)
+//    }
+//  }
 
   val ddls = companies.ddl ++ members.ddl ++ projects.ddl ++ project2Members.ddl
   val stmts = ddls.createStatements ++ ddls.dropStatements
   stmts.foreach(println)
 
-  @DBTransaction def populate()(implicit ev:DbConnectionInfos) {
-    val x = new TableQuery[CompanyTable] {
-      def coucou = println("coucou")
-    }
-    x.coucou
+  @DBTransaction def populate()(implicit ev: DbConnectionInfos) {
     val csize = companies.list.size
+    Company(None, "", "", null, null)
     if (csize == 0) {
-      val typesafeId = companies returning  (companies.map(_.id)) insert(Company(None, "typesafe", "http://www.typesafe.com"))
-      val martinId = members returning (members.map(_.id)) insert(Member(None, "modersky", UserRights.ADMIN, Address(1, "ici", "10001"), typesafeId, None))
-      val szeigerId = members  returning (members.map(_.id)) insert(Member(None, "szeiger", UserRights.GUEST, Address(1, "ici", "10001"), typesafeId, Some(martinId)))
-      println(typesafeId+","+martinId+","+szeigerId)
+      val typesafeId = companies returning (companies.map(_.id)) insert (Company(None, "typesafe", "http://www.typesafe.com"))
+      val martinId = members returning (members.map(_.id)) insert (Member(None, "modersky", UserRights.ADMIN, Address(1, "ici", "10001"), typesafeId, None))
+      val szeigerId = members returning (members.map(_.id)) insert (Member(None, "szeiger", UserRights.GUEST, Address(1, "ici", "10001"), typesafeId, Some(martinId)))
+      println(typesafeId + "," + martinId + "," + szeigerId)
       val slickId = projects.insert(Project(None, "Slick", typesafeId))
       project2Members.insert(Project2Member(slickId, martinId))
       val project = projects.where(_.name === "Slick").first
@@ -49,10 +92,10 @@ object Services {
     val mappedQuery = companies.map(row => (row.name, row.website))
     mappedQuery.update(("newCompanyName", "http://newWebSite.com"))
 
-    val xx =for {
+    val xx = for {
       m <- members
       c <- m.company if c.name === "newCompanyName"
-    } yield(m)
+    } yield (m)
     println(xx.list.size)
 
     val query2 = companies.where(_.id === 1L)
@@ -64,8 +107,9 @@ object Services {
     val project = projects.where(_.name === "Slick").first
 
     //val members = project.mymembers.list
-    members.foreach { m =>
-      m.loadManager.map(println)
+    members.foreach {
+      m =>
+        m.loadManager.map(println)
     }
     project.loadMembers.drop(1).take(1).list.foreach(println)
   }
